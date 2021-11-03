7d881c09fc365184e3105ed7f5d6d5af38c9397b
nikesuputra-win10 <nike.suputra@gmail.com>
2021-11-04 02:59:46 +0800

    Changed mongoDB connection

--- index.js
+++ index.js
@@ -13,21 +13,15 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use('/scrape', scrape)
 app.use('/series', series)
-app.use('/graphql', graphqlHTTP({schema: schema, graphiql: true}))
-
-// Initializes application port and database information
-const PORT = process.env.PORT
-const USER = process.env.MONGODB_USER
-const PASSWORD = process.env.MONGODB_PASSWORD
-const HOST = process.env.MONGODB_HOST
+app.use('/graphql', graphqlHTTP({ schema: schema, graphiql: true }))
 
 // Connect to the database
-mongoose.connect(`mongodb://${USER}:${encodeURIComponent(PASSWORD)}@${HOST}`, {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 mongoose.connection.once('open', () => {
-    console.log("Connected to MongoDB")
+    console.log('Connected to MongoDB')
 })
 
 app.get('/', (req, res) => {
@@ -36,10 +30,4 @@ app.get('/', (req, res) => {
 
 app.get('/favicon.ico', (req, res) => res.status(204))
 
-app.delete('/db', (req, res) => {
-    mongoose.connection.dropDatabase()
-    console.log('Database deleted successfully')
-    res.redirect('/')
-})
-
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
+app.listen(process.env.PORT, () => console.log(`server running on PORT ${process.env.PORT}`))
